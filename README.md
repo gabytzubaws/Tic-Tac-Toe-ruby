31be6c8f72385d720ba6d73ce1d2dfe2adb76eb7
gabytzubaws <gabytzubaws@gmail.com>
2019-08-13 14:14:38 +0300

    It works fine now!

---
+++ README.md
@@ -0,0 +1,3 @@
+Tic-Tac-Toe Object-Oriented Ruby implementation that you can play in commandline.
+
+Granted the implementation is incredibly messy but it works for now, apart from a bug that I can't figure out (if you place first element on position (0,0) the game will end saying (blank) won)
--- tic_tac_toe2.rb
+++ tic_tac_toe2.rb
@@ -1,5 +1,4 @@
 class Board
-
   def initialize (rows, cols)
     @board = Array.new
     for i in 0..rows-1
@@ -9,6 +8,8 @@ class Board
       end
       @board.push(row)
     end
+    puts "-------------------"
+    puts "----Tic-Tac-Toe----"
   end
 
   def board # returns current board
@@ -22,18 +23,46 @@ class Board
     end
   end
 
-  def modify_board(pos, sign)
-    @board[pos[0]][pos[1]] = "[#{sign}]"
+  def modify_board(pos, player)
+    if @board[pos[0]][pos[1]] == '[_]'
+       @board[pos[0]][pos[1]] = "[#{player.sign}]"
+     else
+       puts "That position is already filled! Try again"
+       self.show_board
+       self.modify_board(player.move, player)
+     end
+  end
+
+  def win?
+    current_board = self.board
+    for i in 0..2 do
+      if (current_board[i][0] != '[_]' && current_board[i][0] == current_board[i][1] && current_board[i][1] == current_board[i][2])
+           return current_board[i][0]
+      end
+      if (current_board[0][i] != '[_]' && current_board[0][i] == current_board[1][i] && current_board[1][i] == current_board[2][i])
+          return current_board[0][i]
+      end
+    end
+    if (current_board[0][0] != '[_]' && current_board[0][0] == current_board[1][1] &&  current_board[1][1] == current_board[2][2])
+      return current_board[0][0]
+    end
+
+    if current_board[0][0] != '[_]' && current_board[2][0] == current_board[1][1] && current_board[1][1] == current_board[0][2]
+      return current_board[2][0]
+    end
+    return false
   end
 
 end
 
 class Player
-  def initialize
-    print "Choose a sign(X or O): "
-    sign = gets.chomp.upcase
+  @@players_count = 0
+  def initialize(sign)
+    @@players_count += 1
+    @count = @@players_count
     @sign = sign
-    @name = "Player#{@sign}"
+    @name = "Player#{@count}"
+    puts "Player#{@count} is playing with '#{@sign}'"
   end
 
   def name #name getter
@@ -44,47 +73,76 @@ class Player
     @sign
   end
 
-  def add_move
+  def move
     print "Enter position to enter #{self.sign.upcase} in(row): "
     row = gets.chomp.downcase.to_i
     print "Enter position to enter #{self.sign.upcase} in(column): "
     column = gets.chomp.downcase.to_i
+    #check if posiiton is already filled with 'x' or 'o'
     [row,column]
   end
 
 
 end
 
-=begin
+
 class Game
   def initialize
     @new_board = Board.new(3,3)
-    @player1 = Player.new
-    @player2 = Player.new
-    while(!@new_board.win?)
-
+    @new_board.show_board
+    print "Player1 Choose a sign(X or O): "
+    sign = gets.chomp.upcase
+    @player1 = Player.new(sign)
+    if sign == 'X' ? (sign = 'O') : (sign = 'X')
     end
+    @player2 = Player.new(sign)
+    @turn = true
+    @win = false
+
+    while !@win
+      if @turn
+        @new_board.modify_board(@player1.move, @player1)
+        @new_board.show_board
+        if @turn ? @turn = false : @turn = true
+        end
+      else
+        @new_board.modify_board(@player2.move, @player2)
+        @new_board.show_board
+        if @turn ? @turn = false : @turn = true
+        end
+      end
+      @win = @new_board.win?
+      if @win
+        puts "#{@win} Has won the game!"
+      end
+    end
+
   end
 
   def get_board
-    @new_board
+    @new_board.board
   end
 
   def win?
     # 9 possible win scenarios
     to_return = false
-
   end
 
 end
-=end
 
-new_board = Board.new(3,3)
+Game.new()
+
+
+
+
+
+
+#new_board = Board.new(3,3)
 #puts new_board.board.inspect
-new_board.show_board
-player1 = Player.new
-player2 = Player.new
-new_board.modify_board(player1.add_move, player1.sign)
-new_board.show_board
-new_board.modify_board(player2.add_move, player2.sign)
-new_board.show_board
+#new_board.show_board
+#player1 = Player.new
+#player2 = Player.new
+#new_board.modify_board(player1.add_move, player1.sign)
+#new_board.show_board
+#new_board.modify_board(player2.add_move, player2.sign)
+#new_board.show_board
